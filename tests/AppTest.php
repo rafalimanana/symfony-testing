17bcab27386848e2f6ae64701c5328a2b4969f6d
rafalimanana <rafalimananafabrice@gmail.com>
2022-07-13 09:54:08 +0300

    first commit

---
+++ tests/AppTest.php
@@ -0,0 +1,14 @@
+<?php
+namespace App\Tests;
+
+use PHPUnit\Framework\TestCase;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+class AppTest extends TestCase {
+
+	public function testTestsAreWorking()
+	{
+		// $this->assertEquals(2, 1+1);
+	}
+	
+}
